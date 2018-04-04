f3fc3b2938650d4edc073f8ffe4389d673436c4c
James Schumann <jschumann@gmail.com>
2018-04-04 14:57:09 -0400

    connect to graph db [wip]

--- event_reactor/index.js
+++ event_reactor/index.js
@@ -1,7 +1,28 @@
 module.exports = function (context, eventGridEvent) {
+    var Gremlin = require('gremlin');
+    var async = require('async');
+
+    const client = Gremlin.createClient(
+        443,
+        process.env[relayGraphURL],
+        {
+            "session": false,
+            "ssl": true,
+            "user": `/dbs/${process.env[relayGraphDatabase]}/colls/${process.env[relayGraphCollection]}`,
+            "password": process.env[relayGraphPrimaryKey]
+        }
+    );
+
     context.log("JavaScript Event Grid function processed a request.");
     context.log("Subject: " + eventGridEvent.subject);
     context.log("Time: " + eventGridEvent.eventTime);
     context.log("Data: " + JSON.stringify(eventGridEvent.data));
     context.done();
+
+    client.execute("g.V().count()", { }, (err, results) => {
+        if (err) {
+            return callback(console.error(err));
+        }
+        console.log("Result: %s\n", JSON.stringify(results));
+    });
 };
