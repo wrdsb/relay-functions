e04954fee903bf5a619ef22ca0f6e8497ef7d798
James Schumann <jschumann@gmail.com>
2018-04-06 10:43:03 -0400

    stub event_patch

---
+++ event_patch/README.md
@@ -0,0 +1,2 @@
+# Event Patch
+Performs a partial update of the details of an event (vertex) in the Relay database. Only included properties are updated.
---
+++ event_patch/index.js
@@ -0,0 +1,47 @@
+module.exports = function (context, data) {
+    var Gremlin = require('gremlin');
+    var async = require('async');
+
+    var event = {};
+    event.id = data.id;
+
+    const client = Gremlin.createClient(
+        443,
+        process.env["relayGraphURL"],
+        {
+            "session": false,
+            "ssl": true,
+            "user": `/dbs/${process.env["relayGraphDatabase"]}/colls/${process.env["relayGraphCollection"]}`,
+            "password": process.env["relayGraphPrimaryKey"]
+        }
+    );
+
+    async.waterfall([
+        function(callback) {
+            callback(null, client, event);
+        },
+        function(client, event, callback) {
+            client.execute(`g.V().has('id', '${event.id}')`, { }, (err, results) => {
+                if (err) {
+                    callback(err);
+                } else {
+                    event = results[0];
+                    callback(null, event);
+                }
+            });
+        },
+    ], function (err, event) {
+        if (err) {
+            context.done(err);
+        } else {
+            context.res = {
+                status: 200,
+                body: event
+            };
+            event = JSON.stringify(event);
+            context.bindings.outBlob = event;
+            context.log(event);
+            context.done(null, event);
+        }
+    });
+};
